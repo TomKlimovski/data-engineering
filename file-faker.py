77d3398830f3992c8b0ced8d05b8e77a3a676eda
Tom Klimovski <tom.klimovski@gmail.com>
2022-11-29 16:26:19 +1100

    coupla changes

--- file-faker.py
+++ file-faker.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import os
+import json
 
 fake = Faker()
 
@@ -10,15 +11,19 @@ K, M, G = 1024, 1024, 1024
 # 1 megabyte
 DEFAULT_MAXIMUM_FILE_SIZE = 1 * K * M
 
-Faker.seed(0)
+# Faker.seed(0)
 fake_json_list = []
-for _ in range(5):
-    fake_json_list.append(fake.json(data_columns={'ID': 'pyint',
-                                                  'Details': {'Name': 'name', 'Address': 'address'}}, num_rows=2))
 
 # print(fake_json_list)
 with open("data.json", "w") as f:
-    f.write(''.join(fake_json_list))
+    while(os.stat("data.json").st_size < 64):
+        fake_json_list.append(fake.json(data_columns={'ID': 'pyint',
+                                        'Details': {'Name': 'name', 'Address': 'address'}}, num_rows=1))
+        # strip brackets from list
+        data = fake_json_list[1:-1]
+        print(type(data))
+        f.write(''.join(fake_json_list))
+
 
 f.close()
 
